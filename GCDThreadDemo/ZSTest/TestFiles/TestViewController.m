123d557020dffbacf1dddd234fa181e1cc8200a3
zhoushuai <zhoushuai@qinqimafang.cn>
2018-04-11 11:02:16 +0800

    测试NSOperationQueue

--- GCDThreadDemo/ZSTest/TestFiles/TestViewController.m
+++ GCDThreadDemo/ZSTest/TestFiles/TestViewController.m
@@ -126,6 +126,41 @@
 }
 
 
+//9.测试NSOperationQueue
+- (void)testOperationQueue{
+    // 1.创建队列
+    NSOperationQueue *queue = [[NSOperationQueue alloc] init];
+    // 2.创建操作
+    // 使用 NSInvocationOperation 创建操作1
+    NSInvocationOperation *op1 = [[NSInvocationOperation alloc] initWithTarget:self selector:@selector(task1) object:nil];
+
+    // 使用 NSInvocationOperation 创建操作2
+    NSInvocationOperation *op2 = [[NSInvocationOperation alloc] initWithTarget:self selector:@selector(task2) object:nil];
+
+    // 使用 NSBlockOperation 创建操作3
+    NSBlockOperation *op3 = [NSBlockOperation blockOperationWithBlock:^{
+        for (int i = 0; i < 10; i++) {
+            [NSThread sleepForTimeInterval:2]; // 模拟耗时操作
+            NSLog(@"3---%@", [NSThread currentThread]); // 打印当前线程
+        }
+    }];
+    
+    // 3.使用 addOperation: 添加所有操作到队列中
+    [queue addOperation:op1]; // [op1 start]
+    [queue addOperation:op2]; // [op2 start]
+    [queue addOperation:op3]; // [op3 start]
+}
+
+- (void)task1{
+    NSLog(@"1---%@", [NSThread currentThread]); // 打印当前线程
+}
+
+- (void)task2{
+    NSLog(@"2---%@", [NSThread currentThread]); // 打印当前线程
+}
+
+
+
 #pragma mark - Getter && Setter
 //创建串行队列,传入参数为DISPATCH_QUEUE_SERIAL
 - (dispatch_queue_t)serialQueue{
